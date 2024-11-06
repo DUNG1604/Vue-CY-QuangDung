6a49fbd2a04e336267f9308402d2ece297b8cd49
DUNG1604 <dung0373691479@gmail.com>
2024-11-06 11:54:44 +0700

    1st

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,9 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
   <div>
-    <div class="flex flex-col items-center mt-[10px]">
-      <div class="flex gap-[10px]">
-        <RouterLink to="/section1">
-          <button class="p-[5px] border-[1px] rounded-[10px] cursor-pointer">
-            Section 1
-          </button>
-        </RouterLink>
-        <RouterLink to="/section2">
-          <button class="p-[5px] border-[1px] rounded-[10px] cursor-pointer">
-            Section 2
-          </button>
-        </RouterLink>
-        <RouterLink to="/section3">
-          <button class="p-[5px] border-[1px] rounded-[10px] cursor-pointer">
-            Section 3
-          </button>
-        </RouterLink>
-        <RouterLink to="/section4">
-          <button class="p-[5px] border-[1px] rounded-[10px] cursor-pointer">
-            Section 4
-          </button>
-        </RouterLink>
-      </div>
-    </div>
-    <RouterView />
+    hello
   </div>
 </template>
 
